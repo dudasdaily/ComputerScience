82c2b31ad89b936b58f9e2669b03a6520feb672a
dudasdaily <dudasdaily@gmail.com>
2024-10-02 16:49:06 +0900

    예제02 생성

--- 2학년_2학기/웹프로그래밍/chap03/src/main/java/com/web/chap03/chap03/MyController.java
+++ 2학년_2학기/웹프로그래밍/chap03/src/main/java/com/web/chap03/chap03/MyController.java
@@ -29,4 +29,30 @@ public class MyController {
         return "ex01Answer";
     }
 
+    @GetMapping("/ex02")
+    public String ex02()
+    {
+        return "ex02";
+    }
+    
+    @PostMapping("/ex02/answer")
+    public String ex02Answer(@RequestParam(name="name") String name, @RequestParam(name="po") String po, Model mo)
+    {
+        mo.addAttribute("name", name);
+        mo.addAttribute("po", po);
+
+        int salary = 0;
+
+        switch(po)
+        {
+            case "사원" -> salary = 3000;
+            case "대리" -> salary = 5000;
+            case "팀장" -> salary = 7000;
+            case "임원" -> salary = 9000;
+        }
+
+        mo.addAttribute("salary", salary);
+        return "ex02Answer";
+    }
+
 }
