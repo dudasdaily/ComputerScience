5d9a5675e234293d11b8b1058449ce433d862905
dudasdaily <dudasdaily@gmail.com>
2024-10-22 17:37:16 +0900

    예제3 생성

--- 2학년_2학기/웹프로그래밍/중간고사/chap03/src/main/java/web/chap03/MyController.java
+++ 2학년_2학기/웹프로그래밍/중간고사/chap03/src/main/java/web/chap03/MyController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Controller
 public class MyController {
@@ -31,7 +29,6 @@ public class MyController {
         return "ex02";
     }
     
-    
     @PostMapping("/ex02/answer")
     public String ex02Answer(@RequestParam("name") String name, 
                              @RequestParam("position") String position, Model mo) {
@@ -49,7 +46,18 @@ public class MyController {
 
         return "ex02Answer";
     }
-    
-    
 
+    @GetMapping("/ex03")
+    public String ex03() {
+        return "ex03";
+    }
+
+    @PostMapping("/ex03/answer")
+    public String ex03Answer(@RequestParam("name") String name,
+                            @RequestParam("color") String color, Model mo) {
+        
+        mo.addAttribute("name", name);
+        mo.addAttribute("color", color);
+        return "ex03Answer";
+    }
 }
