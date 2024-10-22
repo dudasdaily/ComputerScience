75f372a7a5649b3bcc43ca899ffc0da7dbfbbc97
dudasdaily <dudasdaily@gmail.com>
2024-10-22 17:00:48 +0900

    예제1 생성

--- 2학년_2학기/웹프로그래밍/중간고사/chap03/src/main/java/web/chap03/MyController.java
+++ 2학년_2학기/웹프로그래밍/중간고사/chap03/src/main/java/web/chap03/MyController.java
@@ -1,9 +1,10 @@
 package web.chap03;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyController {
@@ -11,6 +12,18 @@ public class MyController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/ex01")
+    public String ex01() {
+        return "ex01";
+    }
+
+    @PostMapping("/ex01/answer")   
+    public String ex01Answer(@RequestParam(name = "id") String id, Model mo) {
+        mo.addAttribute("id", id);       
+        return "ex01Answer";
+    }
     
     
+
 }
