dbdb636c33781b9306ab2d654f3282f964690141
dudasdaily <dudasdaily@gmail.com>
2024-11-02 15:58:08 +0900

    임시완성

--- 2학년_2학기/파일처리론/replacement_2143030.py
+++ 2학년_2학기/파일처리론/replacement_2143030.py
@@ -1,3 +1,4 @@
+# TO-DO : 병합 과정에서 리스트를 생성하지 않고 바로 파일에 적는 merge_sort() 구현
 buffer_size = 5
 
 input_path = "C:/DUDA/대학/2학년 2학기/파일처리론/파일처리론 과제/replacement_input.txt"
@@ -23,13 +24,42 @@ def find_min(buffer: list):
 
     return min_index
 
+def merge_sorted_lists(list1, list2):
+    merged = []
+    i, j = 0, 0
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+
+    # 남은 요소 추가
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    
+    return merged
+
+def merge_sort(runs: list) -> list:
+    if len(runs) == 0:
+        return []
+    if len(runs) == 1:
+        return runs[0]
+
+    mid = len(runs) // 2
+    left = merge_sort(runs[:mid])
+    right = merge_sort(runs[mid:])
+
+    return merge_sorted_lists(left, right)
+
 write_file = open(output_path, "w")
 
 with open(input_path, "r") as read_file:
     cases_num = int(read_file.readline())
 
-    for _ in range(cases_num):
-        # buffer = [None] * buffer_size
+    for n in range(cases_num):
         buffer = []
         runs = [[]]
         runs_idx = 0
@@ -96,7 +126,22 @@ with open(input_path, "r") as read_file:
                 current_num = buffer.pop(min_idx).value
                 runs[runs_idx].append(current_num)
         
-        # write_file.write(str(len(runs)) + "\n")
-        print(runs)
+        # 생성된 Runs 숫자 쓰기
+        write_file.write(str(len(runs)) + "\n")
+        
+        # Runs를 병합정렬한 리스트 쓰기
+        merged = merge_sort(runs)
+
+        for idx in range(len(merged)):
+            if idx != len(merged) - 1:
+                write_file.write(str(merged[idx]) + " ")
+            
+            else:
+                write_file.write(str(merged[idx]))
+
+        if n != cases_num - 1:
+            write_file.write("\n")
+            
+
 
 write_file.close()
