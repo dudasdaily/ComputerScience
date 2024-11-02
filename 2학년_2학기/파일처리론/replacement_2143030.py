c0331ee57b717396c65d317a04f61e35c86ef548
dudasdaily <dudasdaily@gmail.com>
2024-11-02 16:37:37 +0900

    merge과정 필요없음을 확인 후 수정

--- 2학년_2학기/파일처리론/replacement_2143030.py
+++ 2학년_2학기/파일처리론/replacement_2143030.py
@@ -1,8 +1,9 @@
-# TO-DO : 병합 과정에서 리스트를 생성하지 않고 바로 파일에 적는 merge_sort() 구현
+from pathlib import Path
+
 buffer_size = 5
 
-input_path = "C:/DUDA/대학/2학년 2학기/파일처리론/파일처리론 과제/replacement_input.txt"
-output_path = "C:/Users/dudas/OneDrive/바탕 화면/test.txt"
+input_path = Path(__file__).parent / 'replacement_input.txt'
+output_path = Path(__file__).parent / 'replacement_output.txt'
 
 # 파일값의 최대값(나중에 버퍼에서 최솟값을 찾는 함수에서 최소값의 초기값을 정할때 씀)
 max_value = 100
@@ -23,36 +24,6 @@ def find_min(buffer: list):
             min_index = i
 
     return min_index
-
-def merge_sorted_lists(list1, list2):
-    merged = []
-    i, j = 0, 0
-    
-    while i < len(list1) and j < len(list2):
-        if list1[i] <= list2[j]:
-            merged.append(list1[i])
-            i += 1
-        else:
-            merged.append(list2[j])
-            j += 1
-
-    # 남은 요소 추가
-    merged.extend(list1[i:])
-    merged.extend(list2[j:])
-    
-    return merged
-
-def merge_sort(runs: list) -> list:
-    if len(runs) == 0:
-        return []
-    if len(runs) == 1:
-        return runs[0]
-
-    mid = len(runs) // 2
-    left = merge_sort(runs[:mid])
-    right = merge_sort(runs[mid:])
-
-    return merge_sorted_lists(left, right)
 
 write_file = open(output_path, "w")
 
@@ -128,20 +99,16 @@ with open(input_path, "r") as read_file:
         
         # 생성된 Runs 숫자 쓰기
         write_file.write(str(len(runs)) + "\n")
-        
-        # Runs를 병합정렬한 리스트 쓰기
-        merged = merge_sort(runs)
 
-        for idx in range(len(merged)):
-            if idx != len(merged) - 1:
-                write_file.write(str(merged[idx]) + " ")
+        for i in range(len(runs)):
+            for j in range(len(runs[i])):
+                if j != (len(runs[i]) - 1):
+                    write_file.write(str(runs[i][j]) + " ")
+                
+                elif j == (len(runs[i]) - 1) and i != len(runs):
+                    write_file.write(str(runs[i][j]) + "\n")
+                
+                else:
+                    write_file.write(str(runs[i][j]))
             
-            else:
-                write_file.write(str(merged[idx]))
-
-        if n != cases_num - 1:
-            write_file.write("\n")
-            
-
-
 write_file.close()
