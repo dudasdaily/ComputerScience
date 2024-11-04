2c03833cb1785c56c87d519dc90f2e1aed922ad1
dudasdaily <dudasdaily@gmail.com>
2024-11-04 14:08:54 +0900

    웹프 4장

---
+++ 2학년_2학기/웹프로그래밍/chap04/src/main/java/web/chap04/Member.java
@@ -0,0 +1,12 @@
+package web.chap04;
+
+public class Member {
+    public String mid;
+    public String pw;
+
+    public Member(String mid, String pw) {
+        this.mid = mid;
+        this.pw = pw;
+    }
+
+}
