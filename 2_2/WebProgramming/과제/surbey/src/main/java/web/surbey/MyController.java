aba0858f79b5e2d90db47315eaa7843d47ff585c
dudasdaily <dudasdaily@gmail.com>
2024-11-25 13:49:53 +0900

    웹프 surbey

---
+++ 2_2/WebProgramming/과제/surbey/src/main/java/web/surbey/MyController.java
@@ -0,0 +1,58 @@
+package web.surbey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class MyController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/surbey")
+    public String surbey(HttpSession se, @RequestParam("id") String id) {
+        se.setAttribute("id", id);
+        return "surbey";
+    }
+
+    @PostMapping("/surbey2")
+    public String surbey2(HttpSession se, @RequestParam("color") String color) {
+        se.setAttribute("color", color);
+        return "surbey2";
+    }
+
+    @PostMapping("/lipservice")
+    public String postMethodName(HttpSession se,@RequestParam("os") String os) {
+        se.setAttribute("os", os);
+
+        return "lipservice";
+    }
+
+    @GetMapping("/result")
+    public String result(HttpSession se, Model mo) {
+        String id = (String)se.getAttribute("id");
+        String color = (String)se.getAttribute("color");
+        String os = (String)se.getAttribute("os");
+        String f_color = "";
+
+        switch (color) {
+            case "빨간색" -> f_color = "red";
+            case "파란색" -> f_color = "blue";
+        }
+
+        mo.addAttribute("id", id);
+        mo.addAttribute("f_color", f_color);
+        mo.addAttribute("color", color);
+        mo.addAttribute("os", os);
+        
+        return "result";
+    }
+}
