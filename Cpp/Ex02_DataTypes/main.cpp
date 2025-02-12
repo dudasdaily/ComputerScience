6d5911f325f6120c02eaa6b277105ab78b379123
dudasdaily <dudasdaily@gmail.com>
2025-02-13 02:59:21 +0900

    테스트

---
+++ Cpp/Ex02_DataTypes/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i; // 변수를 정의할 때 자료형을 미리 지정
+    i = 123; // 변수 명 == 메모리 주소 (객체 레퍼런스 아님)    
+
+    // * sizeof
+    sizeof(i);
+
+    return 0;
+}
