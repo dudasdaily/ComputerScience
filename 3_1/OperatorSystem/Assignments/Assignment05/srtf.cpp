b4b76038be0619e2ee5959d75da35333934ca4c0
dudasdaily <dudasdaily@gmail.com>
2025-05-08 03:25:47 +0900

    srtf 최종

--- 3_1/OperatorSystem/Assignments/Assignment05/srtf.cpp
+++ 3_1/OperatorSystem/Assignments/Assignment05/srtf.cpp
@@ -1,185 +1,306 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <algorithm>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int total_time = 0;
+int idle_time = 0;
+int finishedProc = 0;
 
 class Job
 {
 public:
-    int cpu_job;
-    int io_job;
+    int cpuTime;
+    int ioTime;
 };
 
 class Process
 {
 public:
     int pid;
-    int enter_time;
-    int rq_enter_time;
-    vector<Job> jobs;
+    int rqEnterTime;
+    int finishedTime;
 
     Job job;
+    vector<Job> jobs;
 
     void cpuWork()
     {
-        job.cpu_job--;
+        job.cpuTime--;
     }
 
     void ioWork()
     {
-        job.io_job--;
+        job.ioTime--;
+    }
+
+    void refreshJob()
+    {
+        if (!jobs.empty())
+        {
+            job = jobs.front();
+            jobs.erase(jobs.begin());
+        }
     }
 };
 
+unordered_map<int, vector<Process *>> processArrivalTime;
+vector<Process *> processes;
+vector<Process *> io_q;
+
 struct Compare
 {
-    bool operator()(const Process *a, const Process *b) const
+    const bool operator()(const Process *a, const Process *b)
     {
-        if (a->job.cpu_job != b->job.cpu_job)
-            return a->job.cpu_job > b->job.cpu_job; // a가 크면 -> b보다 우선순위가 낮음..
+        if (a->job.cpuTime != b->job.cpuTime)
+            return a->job.cpuTime > b->job.cpuTime;
 
-        if (a->enter_time != b->enter_time)
-            return a->enter_time > b->enter_time;
+        if (a->rqEnterTime != b->rqEnterTime)
+            return a->rqEnterTime > b->rqEnterTime;
 
         return a->pid > b->pid;
     }
 };
 
-// 입력 파일의 첫 줄을 읽음
-int readFirstLine(ifstream &input_file)
+priority_queue<Process *, vector<Process *>, Compare> rq; // 레디큐
+
+int readFirstInt(ifstream &inputFile)
 {
     string line;
-    getline(input_file, line);
+    getline(inputFile, line);
 
     return stoi(line);
 }
 
-Process *setProcess(int pid, ifstream &input_file, unordered_map<int, vector<Process *>> &enter_map)
+void setProcessJob(Process *&proc, stringstream &line)
 {
-    Process *p = new Process();
-
-    p->pid = pid;
-
-    string line;
-    getline(input_file, line);
-
-    stringstream ss(line);
-
-    ss >> p->enter_time;
-
-    int data, i = 0;
+    int data;
 
     Job job;
-    bool has_io;
 
-    for (int i = 0; ss >> data; i++)
+    int i = 0;
+
+    while (line >> data)
     {
         if (data == -1)
-        {
-            if (!has_io)
-            {
-                job.io_job = 0;
-                p->jobs.push_back(job);
-            }
-
             break;
-        }
 
         if (i % 2 == 0)
         {
-            job.cpu_job = data;
-            has_io = false;
+            job.cpuTime = data;
         }
 
         if (i % 2 != 0)
         {
-            job.io_job = data;
-            has_io = true;
-
-            p->jobs.push_back(job);
+            job.ioTime = data;
+            proc->jobs.push_back(job);
         }
+
+        i++;
     }
 
-    p->job = p->jobs[0];
-    p->jobs.erase(p->jobs.begin());
+    if (i % 2 != 0)
+    {
+        job.ioTime = 0;
+        proc->jobs.push_back(job);
+    }
 
-    enter_map[p->enter_time].push_back(p);
-    return p;
+    proc->refreshJob();
 }
 
-void checkEnterTime(int t, unordered_map<int, vector<Process *>> &enter_map,
-                    priority_queue<Process *, vector<Process *>, Compare> &rq)
+// set process's pid, push process to processArrivalTime map!
+Process *createProcess(int pid, stringstream &line)
 {
-    if (enter_map.count(t))
+    Process *proc = new Process();
+    proc->pid = pid; // set pid
+
+    int arrivalTime;
+    line >> arrivalTime;
+
+    processArrivalTime[arrivalTime].push_back(proc);
+    setProcessJob(proc, line);
+
+    return proc;
+}
+
+void pushArrivedProc()
+{
+    if (processArrivalTime.count(total_time))
     {
-        for (Process *p : enter_map[t])
+        for (Process *p : processArrivalTime[total_time])
         {
-            p->rq_enter_time = t;
+            p->rqEnterTime = total_time;
             rq.push(p);
         }
     }
 }
 
-void ioWork(priority_queue<Process *, vector<Process *>, Compare> &rq,
-            vector<Process *> &io_q)
+Process *scheduling()
 {
-}
-
-void cpuWork(priority_queue<Process *, vector<Process *>, Compare> &rq,
-             vector<Process *> &io_q)
-{
-    Process *current = rq.top();
+    Process *proc = rq.top();
     rq.pop();
 
-    current->cpuWork();
+    return proc;
+}
+
+void eraseProc(Process *&proc, vector<Process *> &v)
+{
+    auto it = find(v.begin(), v.end(), proc);
+
+    if (it != v.end())
+        v.erase(it);
+}
+
+void pushToReadyQueueIfIoDone()
+{
+    vector<Process *> removeList;
+    for (Process *p : io_q)
+    {
+        // IO작업을 다 끝냈으면
+        if (p->job.ioTime <= 0)
+        {
+            removeList.push_back(p);
+
+            // 프로세스 종료
+            if (p->jobs.empty())
+            {
+                p->finishedTime = total_time;
+                finishedProc++;
+            }
+
+            else
+            {
+                p->refreshJob();
+                p->rqEnterTime = total_time;
+                rq.push(p);
+            }
+        }
+    }
+
+    for (Process *p : removeList)
+    {
+        eraseProc(p, io_q);
+    }
+}
+
+void pushToIoQueueIfCpuDone(Process *&current)
+{
+    if (current->job.cpuTime <= 0)
+    {
+        bool isCurrentFinished = current->job.ioTime <= 0 && current->jobs.empty();
+        if (isCurrentFinished)
+        {
+            current->finishedTime = total_time;
+            finishedProc++;
+
+            return;
+        }
+
+        if (current->job.ioTime > 0)
+        {
+            io_q.push_back(current);
+        }
+    }
+}
+
+void WorkIoQueue()
+{
+    for (Process *p : io_q)
+    {
+        p->ioWork();
+    }
+}
+
+void printInfo(Process *&p)
+{
+    printf("Time=%d selectedProcess=%d idleTime=%d\n", total_time, p->pid, idle_time);
 }
 
 int main()
 {
-    ifstream input_file;
-    ofstream output_file;
+    ifstream inputFile;
+    ofstream outputFile;
 
-    input_file.open("./srtf.inp");
-    output_file.open("./srtf.out");
+    inputFile.open("./srtf.inp");
+    outputFile.open("./srtf.out");
 
-    if (!input_file.is_open() || !output_file.is_open())
+    if (!inputFile.is_open() || !outputFile.is_open())
     {
-        cout << "파일 열기 중 오류가 발생했습니다. 프로그램을 종료합니다!\n"
-             << endl;
-
+        cout << "파일열기 실패!" << endl;
         return -1;
     }
 
-    int n = readFirstLine(input_file); // 테스트 케이스 횟수
+    int numProcesses = readFirstInt(inputFile);
 
-    vector<Process *> processes;                              // 여러 프로세스들을 담을 리스트
-    unordered_map<int, vector<Process *>> enter_map;          // 큐에 들어갈 시간을 체크 해줄 맵
-    priority_queue<Process *, vector<Process *>, Compare> rq; // 레디 큐
-    vector<Process *> io_q;
-
-    for (int i = 0; i < n; i++)
+    // 프로세스 생성
+    for (int i = 0; i < numProcesses; i++)
     {
-        Process *p = setProcess(i, input_file, enter_map);
+        string lineStr;
+        getline(inputFile, lineStr);
+        stringstream line(lineStr);
 
-        processes.push_back(p);
+        Process *proc = createProcess(i, line);
+        processes.push_back(proc);
     }
 
-    // 메인 루프 시작!
+    // 메인 루프 시작
     while (true)
     {
-        checkEnterTime(total_time, enter_map, rq);
-
-        if (rq.empty() && io_q.empty())
+        if (finishedProc == numProcesses)
             break;
 
-        total_time++;
+        pushArrivedProc();
+
+        // 레디큐가 있는 경우
+        if (!rq.empty())
+        {
+            Process *current = scheduling();
+            current->cpuWork();
+            WorkIoQueue();
+            total_time += 1;
+
+            pushToReadyQueueIfIoDone();
+            pushToIoQueueIfCpuDone(current);
+
+            if (current->job.cpuTime > 0)
+            {
+                // current->rqEnterTime = total_time; // ❌ rqEnterTime 갱신 금지
+                rq.push(current);
+            }
+
+            continue;
+        }
+
+        // 이 밑은 다, 레디큐가 없는 경우임.
+        if (io_q.empty())
+        {
+            idle_time++;
+            total_time++;
+
+            continue;
+        }
+
+        bool isLastProc = (finishedProc == (numProcesses - 1) && io_q.front()->jobs.empty() && io_q.front()->job.ioTime > 0);
+        if (!isLastProc)
+        {
+            idle_time++;
+        }
+
+        WorkIoQueue();
+        total_time += 1;
+        pushToReadyQueueIfIoDone();
+    }
+
+    outputFile << idle_time << endl;
+
+    for (auto p : processes)
+    {
+        outputFile << p->finishedTime << endl;
     }
 
     return 0;
